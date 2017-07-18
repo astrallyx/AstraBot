5c4191ecd4c208647014f607b2564df8325035d2
AndrewJWin <fastroute66@live.com>
2017-07-18 15:06:27 -0500

    First version
    Basic frame.

---
+++ Astra.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require("./Astraconfig");
+
+bot.on('ready', () => {
+  bot.user.setGame("'help | " + bot.guilds.size + ' Servers');
+  console.log("AstraBot Online");
+});
+
+bot.on('reconnecting', () => {
+  console.log('AstraBot reconnecting');
+});
+
+bot.on('guildCreate', guild => {
+  console.log('Joined ' + guild.name + '|' + guild.id + ' and is owned by ' + guild.owner.user.username + '|' + guild.owner.id);
+  bot.user.setGame("'help | " + bot.guilds.size + ' Servers');
+});
+
+bot.on('guildDelete', guild => {
+  console.log('Removed from ' + guild.name + '|' + guild.id + ' and it was owned by ' + guild.owner.user.username + '|' + guild.owner.id);
+  bot.user.setGame("'help | " + bot.guilds.size + ' Servers');
+})
+
+bot.on('message', message => {
+  if(!message.content.includes(config.prefix)) return;
+  let cont = message.content.slice(config.prefix);
+  if (cont.includes('Hi')) return message.reply('hello!');
+});
+
+bot.login(config.token);
